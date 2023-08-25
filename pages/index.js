f0e626807c28298ca81f4e951c9acec8799196b6
wattsmainsanglais <wattsmainsanglais@gmail.com>
2023-08-25 15:23:50 +0200

    adding new components, script tag and css modules

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
 
+
+
 export default function Home() {
   return (
     <div className={styles.container}>
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,13 +1,28 @@
 import Link from 'next/link';
+import Head from 'next/head';
+import Script from 'next/script';
+import Layout from '../../components/Layout'
+
 
 export default function FirstPost() {
     return (
-        <>
-            <h1>First Post</h1>;
+        <Layout>
+            <Head>
+                <title>First Post</title>
+                <Script
+                    src="https://connect.facebook.net/en_US/sdk.js"
+                    strategy="lazyOnload"
+                    onLoad={() =>
+                    console.log(`script loaded correctly, window.FB has been populated`)
+                    }
+                />
+
+            </Head>
+            <h1>First Post</h1>
             <h2>
                 <Link href="/">Back to Home</Link>
             </h2>
-        </>
+        </Layout>
     )
 }
   
